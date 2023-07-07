88881af18a9fe3a6e4c5687b40d84af8b618036d
Jón Prüßmeier <jon.pruessmeier@clockin.de>
2023-07-07 08:19:49 +0200

    feat: script finalized

--- index.js
+++ index.js
@@ -1,29 +1,70 @@
 //const { execAsync } = require("child_process");
 
-const { exec } = require('child_process');
-const { promisify } = require('util');
-const execAsync = promisify(exec);
+const { exec } = require('child_process')
+const { promisify } = require('util')
+const execAsync = promisify(exec)
 
-const commandForCurrentBranch = "git rev-parse --abbrev-ref HEAD";
-const commandForMainBranch = "git symbolic-ref refs/remotes/origin/HEAD";
+const commandForCurrentBranch = 'git rev-parse --abbrev-ref HEAD'
+const commandForMainBranch = 'git remote show origin'
 
-async function getBranchByCommand(command) {
-    try {
-      const { stdout } = await execAsync(command);
-      return stdout.trim();
-    } catch (error) {
-      console.error(`Error at following command: ${command}`, error);
-      throw error;
+async function execute(command) {
+  try {
+    const { stdout } = await execAsync(command)
+    return stdout.trim()
+  } catch (error) {
+    console.error(`Error at following command: ${command}`, error)
+    throw error
+  }
+}
+
+async function getMainBranchName() {
+  const stdout = await execute(commandForMainBranch)
+  const lines = stdout.trim().split('\n')
+  for (const line of lines) {
+    if (line.includes('HEAD branch')) {
+      const branchParts = line.split(':')
+      if (branchParts.length === 2) {
+        const masterBranchName = branchParts[1].trim()
+        return masterBranchName
+      }
     }
   }
 
-async function pullAndMerge(){
-    const currentBranch = await getBranchByCommand(commandForCurrentBranch)
-    console.log('Current Branch:', currentBranch)
-    const mainBranch = await getBranchByCommand(commandForMainBranch)
-    console.log('Main Branch:', mainBranch); // Korrigierter console.log-Aufruf
-    console.log(`Switching to branch ${mainBranch}`)
-    
+  throw new Error('Main branch not found')
+}
+
+async function pullAndMerge() {
+  const currentBranch = await execute(commandForCurrentBranch)
+  console.log('Current Branch:', currentBranch)
+
+
+  let mainBranch
+  try {
+    mainBranch = await getMainBranchName()
+  } catch (error) {
+    console.error(
+      'Something went wrong finding the name of the main branch in your repository.',
+      error,
+    )
+    return
+  }
+  console.log('Main Branch:', mainBranch) // Korrigierter console.log-Aufruf
+
+
+  console.log(`Switching to branch: ${mainBranch}`)
+  await execAsync(`git switch ${mainBranch}`)
+
+
+  console.log(`Pulling changes for branch ${mainBranch}...`)
+  await execAsync('git pull')
+
+
+  console.log(`Switching to branch: ${currentBranch}`)
+  await execAsync(`git switch ${currentBranch}`)
+
+  
+  console.log(`Merging ${mainBranch} into ${currentBranch}`)
+  await execAsync(`git merge ${mainBranch}`)
 }
 
 pullAndMerge()
